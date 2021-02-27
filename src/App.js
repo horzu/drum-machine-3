10cb9868b1b5591105ca99ac323333244134c6d9
horzu <mertsakar@hotmail.com>
2021-02-27 09:58:54 +0300

    first fully fucntioning commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {bankOne, bankTwo} from './components/data';
+import React, {useEffect, useState} from 'react';
+
 
 function App() {
+  const [volume, setVolume] = useState(1);
+  const [displayText, setDisplayText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-info min-vh-100 text-white">
+      <div className="text-center" id="drum-machine">
+        <div id="display">
+          {bankOne.map((clip, idx) => (
+            <Pad key={idx} clip={clip} volume={volume} setDisplayText={setDisplayText}/>
+          ))}
+        </div>
+        <br/>
+        <h4>Volume</h4>
+        <input className="w-50" type="range" name="volume" id="volume" step="0.01" min="0" max="1" value={volume} onChange={(e)=> setVolume(e.target.value)}/>
+        <h3>{displayText}</h3>
+      </div>
     </div>
   );
+}
+
+function Pad ({clip, volume, setDisplayText}) {
+  const [active, setActive] = useState(false);
+
+  useEffect(()=> {
+    document.addEventListener("keydown", handleKeyPress)
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress)
+    }
+  })
+  
+  const handleKeyPress = (e) =>{
+    if(e.keyCode === clip.keyCode){
+      playSound();
+    }
+  }
+
+  const playSound = () => {
+    let audio = document.getElementById(clip.keyTrigger);
+    setActive(true);
+    setTimeout(()=> setActive(false), 200);
+    audio.volume = volume;
+    audio.currentTime = 0;
+    audio.play();
+    setDisplayText(clip.id)
+  }
+  
+  return(
+    <div className={`btn btn-secondary p-4 m-3 ${active && "btn-warning"}`  } onClick={playSound}>
+      <audio src={clip.url} id={clip.keyTrigger} />
+      {clip.keyTrigger}
+    </div>
+  )
 }
 
 export default App;
